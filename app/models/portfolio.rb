41cc47b9aa9427143d035d21c12b91837acebc2b
Guillermo Loaysa <gloaysa@gmail.com>
2017-07-09 01:27:02 +0200

    Implement custom scope for portfolio item

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,3 +1,6 @@
 class Portfolio < ApplicationRecord
   validates_presence_of :title, :body, :main_image, :thumb_image
+  #query to the db using scope
+  scope :angular, -> { where(subtitle: "Angular")}
+  scope :ruby_on_rails, -> { where(subtitle: "Ruby on Rails")}
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+3.times do |topic|
+  Topic.create!(
+  title: "Topic #{topic}"
+  )
+end
+
+puts "3 topics created"
+
 10.times do |blog|
   Blog.create!(
     title: "My Blog Post #{blog}",
-    body: "Bacon ipsum dolor amet biltong ball tip alcatra rump flank capicola landjaeger strip steak salami frankfurter. Jowl turkey bresaola alcatra pig, drumstick doner tenderloin pastrami. Burgdoggen ham hock tri-tip beef, filet mignon ball tip andouille pork loin tail pastrami alcatra tongue. Shank frankfurter sirloin, tongue sausage shoulder meatloaf ham doner meatball drumstick landjaeger cupim pancetta. Picanha sirloin turducken, hamburger t-bone biltong meatball leberkas bresaola."
+    body: "Bacon ipsum dolor amet biltong ball tip alcatra rump flank capicola landjaeger strip steak salami frankfurter. Jowl turkey bresaola alcatra pig, drumstick doner tenderloin pastrami. Burgdoggen ham hock tri-tip beef, filet mignon ball tip andouille pork loin tail pastrami alcatra tongue. Shank frankfurter sirloin, tongue sausage shoulder meatloaf ham doner meatball drumstick landjaeger cupim pancetta. Picanha sirloin turducken, hamburger t-bone biltong meatball leberkas bresaola.",
+    topic_id: Topic.last.id
   )
 end
 
@@ -23,10 +32,20 @@ end
 
 puts "5 skills created"
 
-9.times do |portfolio_item|
+8.times do |portfolio_item|
   Portfolio.create!(
     title: "Porfolio title: #{portfolio_item}",
-    subtitle: "My service",
+    subtitle: "Ruby on Rails",
+    body: "Pig turducken leberkas jerky shoulder, short loin sirloin ham. Sirloin salami pig short loin brisket biltong. Sausage capicola short loin picanha. Pancetta shank fatback jerky rump. Cow beef jowl cupim, meatball fatback pork loin turkey",
+    main_image: "http://via.placeholder.com/600x400",
+    thumb_image: "http://via.placeholder.com/350x200"
+  )
+end
+
+1.times do |portfolio_item|
+  Portfolio.create!(
+    title: "Porfolio title: #{portfolio_item}",
+    subtitle: "Angular",
     body: "Pig turducken leberkas jerky shoulder, short loin sirloin ham. Sirloin salami pig short loin brisket biltong. Sausage capicola short loin picanha. Pancetta shank fatback jerky rump. Cow beef jowl cupim, meatball fatback pork loin turkey",
     main_image: "http://via.placeholder.com/600x400",
     thumb_image: "http://via.placeholder.com/350x200"
